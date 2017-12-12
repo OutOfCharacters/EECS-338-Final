352809dd5efe61a7c380d37b4e5e7ffb7458746d
Jonathan Mertz <walrus365@sbcglobal.net>
2017-12-12 16:05:11 -0500

    fixed formatting

--- Car.java
+++ Car.java
@@ -1,194 +1,194 @@
 import java.awt.Color;
 
 public class Car extends Thread {
-
-    private final int xDestination;
-    private final int yDestination;
-    private final int numberOfSteps;
-    private final Color carColor;
-    
-    private ParkingLot myLot;
-    private int currentX;
-    private int currentY;
-    private boolean moveX;
-    private boolean moveY;
-    private boolean moveDiag;
-    
-    private boolean startFlag = false;
-    private boolean stepFlag = false;
-   
-    
-    public Car(int x, int y, int speed, Color colour, ParkingLot newLot)
+  
+  private final int xDestination;
+  private final int yDestination;
+  private final int numberOfSteps;
+  private final Color carColor;
+  
+  private ParkingLot myLot;
+  private int currentX;
+  private int currentY;
+  private boolean moveX;
+  private boolean moveY;
+  private boolean moveDiag;
+  
+  private boolean startFlag = false;
+  private boolean stepFlag = false;
+  
+  
+  public Car(int x, int y, int speed, Color colour, ParkingLot newLot)
+  {
+    xDestination = x;
+    yDestination = y;
+    numberOfSteps = speed;
+    carColor = colour;
+    myLot = newLot;
+  }
+  
+  public Car(Color colour, ParkingLot newLot)
+  {
+    xDestination = 0;
+    yDestination = 0;
+    numberOfSteps = 0;
+    carColor = colour;
+    myLot = newLot;
+  }
+  
+  //this method is called when the thread is ran, overides the thread class
+  public void run()
+  {
+    //stops when car has reached it's destination, car will then be deleted when it's reference is deleted by parkingLot
+    while(currentX != xDestination || currentY != yDestination)
     {
-        xDestination = x;
-        yDestination = y;
-        numberOfSteps = speed;
-        carColor = colour;
-        myLot = newLot;
-    }
-    
-    public Car(Color colour, ParkingLot newLot)
-    {
-      xDestination = 0;
-      yDestination = 0;
-      numberOfSteps = 0;
-      carColor = colour;
-      myLot = newLot;
-    }
-    
-    //this method is called when the thread is ran, overides the thread class
-    public void run()
-    {
-      //stops when car has reached it's destination, car will then be deleted when it's reference is deleted by parkingLot
-      while(currentX != xDestination || currentY != yDestination)
+      //waits for input from parkingLot signaling a new turn
+      while(!startFlag)
       {
-        //waits for input from parkingLot signaling a new turn
-        while(!startFlag)
+      }
+      startFlag = false;
+      stepFlag = false;
+      
+      //car moves as many steps as it is allocated
+      for(int n = 0; n < numberOfSteps; n++)
+      {
+        
+        //checks each space in the loop
+        int[][] spacesToCheck = nextDestination();
+        for(int i = 0; i < 3; i++)
         {
-        }
-        startFlag = false;
-        stepFlag = false;
-
-        //car moves as many steps as it is allocated
-        for(int n = 0; n < numberOfSteps; n++)
-        {
-          
-          //checks each space in the loop
-          int[][] spacesToCheck = nextDestination();
-          for(int i = 0; i < 3; i++)
+          //waits for signal from ParkingLot for a new step
+          while(!stepFlag)
           {
-            //waits for signal from ParkingLot for a new step
-            while(!stepFlag)
+          }
+          stepFlag = false;
+          
+          //checks if space is currently locked
+          if(!myLot.isLocked(spacesToCheck[i][0], spacesToCheck[i][1]))
+          {
+            //locks current space being looked at, checks if it's occupied.  If yes, put car in space and delete old car reference.
+            myLot.setLock(spacesToCheck[i][0], spacesToCheck[i][1], true);
+            if(!myLot.isOccupied(spacesToCheck[i][0], spacesToCheck[i][1]))
             {
+              myLot.setPosition(spacesToCheck[i][0], spacesToCheck[i][1], this);
+              myLot.setPosition(currentX, currentY, null);
+              currentX = spacesToCheck[i][0];
+              currentY = spacesToCheck[i][1];
             }
-            stepFlag = false;
-            
-            //checks if space is currently locked
-            if(!myLot.isLocked(spacesToCheck[i][0], spacesToCheck[i][1]))
-            {
-              //locks current space being looked at, checks if it's occupied.  If yes, put car in space and delete old car reference.
-              myLot.setLock(spacesToCheck[i][0], spacesToCheck[i][1], true);
-              if(!myLot.isOccupied(spacesToCheck[i][0], spacesToCheck[i][1]))
-              {
-                myLot.setPosition(spacesToCheck[i][0], spacesToCheck[i][1], this);
-                myLot.setPosition(currentX, currentY, null);
-                currentX = spacesToCheck[i][0];
-                currentY = spacesToCheck[i][1];
-              }
-              myLot.setLock(spacesToCheck[i][0], spacesToCheck[i][1], false);
-            }
+            myLot.setLock(spacesToCheck[i][0], spacesToCheck[i][1], false);
           }
         }
       }
-      myLot.increment();
     }
+    myLot.increment();
+  }
+  
+  public Color getColor(){
+    return carColor;
+  }
+  public void triggerStartFlag()
+  {
+    startFlag = true;
+  }
+  
+  public void triggerStepFlag()
+  {
+    stepFlag = true;
+  }
+  
+  public int getX(){
+    return this.xDestination;
+  }
+  
+  public int getY(){
+    return this.yDestination;
+  }
+  
+  public int getSteps(){
+    return this.numberOfSteps;
+  }
+  
+  public void setCurrentSpace(int x, int y){
+    currentX = x;
+    currentY = y;
+  }
+  
+  private int xPathLength(){
+    return currentX - xDestination;
+  }
+  
+  private int yPathLength(){
+    return currentY - yDestination;
+  }
+  //helps to decide a general direction
+  private int decideShortestPath(){
+    int xLength = Math.abs(xPathLength());
+    int yLength = Math.abs(yPathLength());
+    int difference = xLength - yLength;
+    //move in the x direction
+    if(difference>0){
+      return 1;
+    }
+    //move in the y direction
+    else if(difference<0){
+      return -1;
+    }
+    //diagonal case
+    else{
+      return 0;
+    }
+  }
+  
+  //check where to go
+  private int[][] nextDestination(){
+    //current location check 3 directions 
+    int directionType = decideShortestPath();
+    int north = getY()-1;
+    int south = getY()+1;
+    int east = getX()+1;
+    int west = getX()-1;
     
- public Color getColor(){
-  return carColor;
- }
-    public void triggerStartFlag()
-    {
-      startFlag = true;
+    //north
+    if(yPathLength()>0 && directionType == -1){
+      //within the bounds + available space
+      //default case: Go straight up
+      return new int[][] {{getX(), north}, {east, north}, {west, north}};
     }
-    
-    public void triggerStepFlag()
-    {
-      stepFlag = true;
+    //south
+    else if(yPathLength()<0 && directionType == -1){
+      return new int[][] {{getX(), south},{east, south}, {west, south}};           
     }
-    
-    public int getX(){
-        return this.xDestination;
+    //east
+    else if(xPathLength()>0 && directionType == 1){
+      //default case go east
+      return new int[][]{{east,getY()},{east,south},{east,north}};
     }
-    
-    public int getY(){
-        return this.yDestination;
+    //west
+    else if(xPathLength()<0 && directionType == 1){
+      //default case go west
+      return new int[][] {{west, getY()},{west, south}, {west, north}};
+    }      
+    //northeast
+    else if(yPathLength()>0 && xPathLength()>0 && directionType == 0){
+      //default case: northeast
+      return new int[][] {{east, north}, {getX(), north}, {east, getY()}};                     
     }
-    
-    public int getSteps(){
-        return this.numberOfSteps;
+    //northwest
+    else if(yPathLength()>0 && xPathLength()<0 && directionType == 0){
+      //check northwest
+      return new int[][]{{west, north},{getX(), north}, {west, getY()}};
     }
-    
-    public void setCurrentSpace(int x, int y){
-        currentX = x;
-        currentY = y;
+    //southeast
+    else if(yPathLength()<0 && xPathLength()>0 && directionType == 0){
+      //default check southeast
+      return new int[][]{{east, south},{getX(), south}, {east, getY()}};
     }
-    
-    private int xPathLength(){
-        return currentX - xDestination;
-    }
-    
-    private int yPathLength(){
-        return currentY - yDestination;
-    }
-    //helps to decide a general direction
-    private int decideShortestPath(){
-        int xLength = Math.abs(xPathLength());
-        int yLength = Math.abs(yPathLength());
-        int difference = xLength - yLength;
-        //move in the x direction
-        if(difference>0){
-            return 1;
-        }
-        //move in the y direction
-        else if(difference<0){
-            return -1;
-        }
-        //diagonal case
-        else{
-            return 0;
-        }
-    }
-    
-    //check where to go
-    private int[][] nextDestination(){
-        //current location check 3 directions 
-        int directionType = decideShortestPath();
-        int north = getY()-1;
-        int south = getY()+1;
-        int east = getX()+1;
-        int west = getX()-1;
-        
-        //north
-        if(yPathLength()>0 && directionType == -1){
-            //within the bounds + available space
-            //default case: Go straight up
-            return new int[][] {{getX(), north}, {east, north}, {west, north}};
-        }
-        //south
-        else if(yPathLength()<0 && directionType == -1){
-            return new int[][] {{getX(), south},{east, south}, {west, south}};           
-        }
-        //east
-        else if(xPathLength()>0 && directionType == 1){
-            //default case go east
-            return new int[][]{{east,getY()},{east,south},{east,north}};
-        }
-        //west
-        else if(xPathLength()<0 && directionType == 1){
-            //default case go west
-            return new int[][] {{west, getY()},{west, south}, {west, north}};
-        }      
-         //northeast
-        else if(yPathLength()>0 && xPathLength()>0 && directionType == 0){
-            //default case: northeast
-            return new int[][] {{east, north}, {getX(), north}, {east, getY()}};                     
-        }
-         //northwest
-        else if(yPathLength()>0 && xPathLength()<0 && directionType == 0){
-            //check northwest
-            return new int[][]{{west, north},{getX(), north}, {west, getY()}};
-        }
-         //southeast
-        else if(yPathLength()<0 && xPathLength()>0 && directionType == 0){
-            //default check southeast
-            return new int[][]{{east, south},{getX(), south}, {east, getY()}};
-        }
-         //southwest
-         //else if(yPathLength()<0 && xPathLength()<0 && directionType == 0)
-        else{
-            //check southwest
-            return new int[][]{{west, south},{getX(), south},{west, getY()}};
-        }              
-    }
-    
+    //southwest
+    //else if(yPathLength()<0 && xPathLength()<0 && directionType == 0)
+    else{
+      //check southwest
+      return new int[][]{{west, south},{getX(), south},{west, getY()}};
+    }              
+  }
+  
 }
--- Controller.java
+++ Controller.java
@@ -7,34 +7,34 @@ class Controller
   public static void main(String[] args)
   {
     ParkingLot parkingLot = new ParkingLot();
-
+    
     JFrame frame = new JFrame();   
     JPanel panel = new JPanel();
     frame.add(panel);
-
+    
     int row = 20;
     int col = 20;
-
+    
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setBounds(100, 100, 500, 500);
     panel.setLayout(new GridLayout(row, col));
-
+    
     JLabel[][] grid= new JLabel[row][col];
     for (int i = 0; i < row; i++){
-        for (int j = 0; j < col; j++){
-          if((i + j) % 2 == 0)
-          {
-            grid[i][j] = new JLabel("kevin", SwingConstants.CENTER);
-            grid[i][j].setBackground(Color.red);
-          }
-          else
-          {
-            grid[i][j] = new JLabel("chang", SwingConstants.CENTER);
-            grid[i][j].setBackground(Color.orange);
-          }
-            grid[i][j].setOpaque(true);
-            panel.add(grid[i][j]);
+      for (int j = 0; j < col; j++){
+        if((i + j) % 2 == 0)
+        {
+          grid[i][j] = new JLabel("kevin", SwingConstants.CENTER);
+          grid[i][j].setBackground(Color.red);
         }
+        else
+        {
+          grid[i][j] = new JLabel("chang", SwingConstants.CENTER);
+          grid[i][j].setBackground(Color.orange);
+        }
+        grid[i][j].setOpaque(true);
+        panel.add(grid[i][j]);
+      }
     }
     //grid[0][0].setBackground(Color.red);
     frame.setVisible(true);
@@ -43,20 +43,20 @@ class Controller
   }
   //Animates the replay
   public static void getReplay(ParkingLot parkingLot, JLabel[][] grid){
- Car[][][] replay = parkingLot.getReplay();
-  //puts current snapshot into the JFrame
- for(int i = 0; i<replay.length; i++)
- {
-  for(int j=0; j<replay[0].length; j++)
-  {
-   for(int k=0; k<replay[0][0].length; k++)
-   {
-    grid[j][k].setBackground(replay[i][j][k].getColor());    
+    Car[][][] replay = parkingLot.getReplay();
+    //puts current snapshot into the JFrame
+    for(int i = 0; i<replay.length; i++)
+    {
+      for(int j=0; j<replay[0].length; j++)
+      {
+        for(int k=0; k<replay[0][0].length; k++)
+        {
+          grid[j][k].setBackground(replay[i][j][k].getColor());    
+        }
+      }
+      //(500);
     }
-   }
-   //(500);
   }
- }
   
   
 }
